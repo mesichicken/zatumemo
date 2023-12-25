f55129f242796c48990143f9183991df7f430d70
iizuka-youhei <81732774+iizuka-youhei@users.noreply.github.com>
2023-12-25 10:26:33 +0900

    first commit(頭文字F)

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <header>
+    <h1>雑メモ</h1>
+  </header>
+  <main>
+    <div class="memo-channel">
+      <p>メモチャンネル</p>
+    </div>
+    <div clas="memo-list">
+      <p>メモリスト</p>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@import './assets/scss/main.scss';
+</style>

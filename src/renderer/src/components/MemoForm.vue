f4ed818433a343b8a2c7c8866c78a7b727a54819
mesi <mesicharida202@gmail.com>
2024-02-29 19:14:58 +0900

    余分なコメントアウトを削除

--- src/renderer/src/components/MemoForm.vue
+++ src/renderer/src/components/MemoForm.vue
@@ -86,7 +86,6 @@ const onTextChange = () => {
               <span class="ql-formats">
                 <button class="ql-link"></button>
                 <button class="ql-image"></button>
-                <!-- <button class="ql-video"></button> -->
               </span>
               <span class="ql-formats">
                 <button class="ql-clean"></button>
